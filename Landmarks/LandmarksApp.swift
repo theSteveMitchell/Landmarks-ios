5798732fdeb36a426dd7ade4b7d95e7de60567bf
Steve Mitchell <TheSteveMitchell@gmail.com>
2021-03-29 09:37:51 -0400

    part 1: creating and combining views

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Steve Mitchell on 3/28/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

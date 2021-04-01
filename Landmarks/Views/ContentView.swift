b92209ff56fb600728f405d84566143b299c607f
Steve Mitchell <TheSteveMitchell@gmail.com>
2021-03-31 22:33:53 -0400

    part 2: Lists and navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Steve Mitchell on 3/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

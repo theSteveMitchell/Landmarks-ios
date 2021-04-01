65b621eb0484d9cd234a16d22a82d5db8f54195e
Steve Mitchell <TheSteveMitchell@gmail.com>
2021-04-01 13:16:12 -0400

    part 3: Handling User Input

---
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,43 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Steve Mitchell on 3/29/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            
+            List{
+                Toggle(isOn: $showFavoritesOnly, label: {
+                    Text("Show Favorites")
+                })
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .environmentObject(ModelData())
+    }
+}

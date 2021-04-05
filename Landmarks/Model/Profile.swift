36f96437803f0438eb829bc8c4fadf5d9a58a6e5
Steve Mitchell <TheSteveMitchell@gmail.com>
2021-04-05 15:55:28 -0400

    part 7: working with UI controls

---
+++ Landmarks/Model/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Steve Mitchell on 4/5/21.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "g_kumar")
+    
+    enum Season: String, CaseIterable, Codable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+        var id: String { self.rawValue }
+    }
+}
---
+++ Landmarks/Views/Hikes/HikeBadge.swift
@@ -0,0 +1,30 @@
+//
+//  HikeBadge.swift
+//  Landmarks
+//
+//  Created by Steve Mitchell on 4/5/21.
+//
+
+import SwiftUI
+
+struct HikeBadge: View {
+    var name: String
+    
+    var body: some View {
+        VStack {
+            Badge()
+                .frame(width: 300, height: 300)
+                .scaleEffect(1.0/3.0)
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+            Text(name)
+                .font(.caption)
+                .accessibilityLabel("Badge for \(name).")
+        }
+    }
+}
+
+struct HikeBadge_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeBadge(name: "Preview Testing")
+    }
+}
